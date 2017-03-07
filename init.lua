66b144707460860b32ac3a8b809c6529f9f79bff
Jannik Kulesha <s9jakule@stud.uni-saarland.de>
2017-03-07 22:50:44 +0100

    impl fractional steps

--- init.lua
+++ init.lua
@@ -7,6 +7,7 @@
 
 
 local walking_period = 2  -- seconds
+local walking_steps = 1
 
 minetest.register_craftitem("worm:spawnegg", {
   description = "worm",
@@ -18,65 +19,89 @@ minetest.register_craftitem("worm:spawnegg", {
     local pos = pointed_thing.above
     local dir = 0
     local length = 6
-    minetest.set_node(pos, {name = "worm:head", param2 = dir})
+    minetest.set_node(pos, {name = "worm:head_1", param2 = dir})
     local headtimer = minetest.get_node_timer(pos)
     pos.z = pos.z - 1
     for i = 0, length do
       minetest.set_node(pos, {name = "worm:body", param2 = dir})
       pos.z = pos.z - 1
     end
-    minetest.set_node(pos, {name = "worm:tail", param2 = dir})
+    minetest.set_node(pos, {name = "worm:tail_1", param2 = dir})
     local tailtimer = minetest.get_node_timer(pos)
     
     headtimer:set(walking_period, 0)
     tailtimer:set(walking_period, 0)
     
-    --itemstack:take_item()
+    itemstack:take_item()
     return itemstack
   end,
 })
 
+for n = 1, walking_steps do
 
-minetest.register_node("worm:head", {
-  description = "A worms head",
-  paramtype = "light",
-  paramtype2 = "facedir",
-  tiles = {
-    "worm_side.png^[transform1",
-    "worm_side.png^[transform1",
-    "worm_side.png",
-    "worm_side.png",
-    "worm_face.png",
-    "worm_side.png"
-  },
-  groups = {snappy = 3},
-  damage_per_second = 10,
-  on_timer = function(pos, elapsed)
+  local step = function(pos, _)
     local node = minetest.get_node(pos)
-    local facedir = node.param2
-    local dirs = {
-      4, 
-      facedir,
-      (facedir + 1) % 4,
-      (facedir + 2) % 4,
-      (facedir + 3) % 4,
-      5 
-    }
-    for i = 1, 6 do
-      local newpos = vector.add(pos, minetest.facedir_to_dir(dirs[i]))
-      if minetest.registered_nodes[minetest.get_node(newpos).name].buildable_to then
-        minetest.set_node(newpos, {name = "worm:head", param2 = dirs[i]})
-        minetest.set_node(pos, {name = "worm:body", param2 = dirs[i]})
-        local timer = minetest.get_node_timer(newpos)
-        timer:set(walking_period, 0)
-        return false
+    node.name = "worm:head_"..(n+1)
+    minetest.set_node(pos, node)
+    local timer = minetest.get_node_timer(pos)
+    timer:set(walking_period, 0)
+    return false
+  end
+  
+  if n == walking_steps then
+    step = function(pos, _)
+      local node = minetest.get_node(pos)
+      local facedir = node.param2
+      local dirs = {
+        4, 
+        facedir,
+        (facedir + 1) % 4,
+        (facedir + 2) % 4,
+        (facedir + 3) % 4,
+        5 
+      }
+      for i = 1, 6 do
+        local newpos = vector.add(pos, minetest.facedir_to_dir(dirs[i]))
+        if minetest.registered_nodes[minetest.get_node(newpos).name].buildable_to then
+          minetest.set_node(newpos, {name = "worm:head_1", param2 = dirs[i]})
+          minetest.set_node(pos, {name = "worm:body", param2 = dirs[i]})
+          local timer = minetest.get_node_timer(newpos)
+          timer:set(walking_period, 0)
+          return false
+        end
       end
+      -- worm stuck
+      return true
     end
-    -- worm stuck
-    return true
-  end,
-  drop = "",
-})
+  end
+
+
+  minetest.register_node("worm:head_"..n, {
+    description = "A worms head",
+    paramtype = "light",
+    paramtype2 = "facedir",
+    drawtype = "nodebox",
+    node_box = {
+      type = "fixed",
+      fixed = {
+        {-0.5, -0.5, -0.5, 0.5, 0.5, -0.5 + (1 / walking_steps) * n},
+      },
+    },
+    tiles = {
+      "worm_side.png^[transform1",
+      "worm_side.png^[transform1",
+      "worm_side.png",
+      "worm_side.png",
+      "worm_face.png",
+      "worm_side.png"
+    },
+    groups = {snappy = 3},
+    damage_per_second = 10,
+    on_timer = step,
+    drop = "",
+  })
+  
+end
 
 minetest.register_node("worm:body", {
   description = "A worms body part",
@@ -95,11 +120,11 @@ minetest.register_node("worm:body", {
     local newpos = vector.add(pos, minetest.facedir_to_dir(node.param2))
     local newnode = minetest.get_node(newpos)
     if newnode.name == "worm:body" then
-      newnode.name = "worm:tail"
+      newnode.name = "worm:tail_1"
       minetest.set_node(newpos, newnode)
       local timer = minetest.get_node_timer(newpos)
       timer:set(walking_period, 0)
-    elseif newnode.name == "worm:head"  then
+    elseif newnode.name:find("worm:head") == 1  then
       newnode.name = "air"
       minetest.set_node(newpos, newnode)
     end
@@ -107,50 +132,74 @@ minetest.register_node("worm:body", {
   drop = "",
 })
 
-minetest.register_node("worm:tail", {
-  description = "A worms tail",
-  paramtype = "light",
-  paramtype2 = "facedir",
-  tiles = {
-    "worm_side.png^[transform1",
-    "worm_side.png^[transform1",
-    "worm_side.png",
-    "worm_side.png",
-    "worm_side.png",
-    "worm_tail.png"
-  },
-  on_timer = function(pos, elapsed)
+for n = 1, walking_steps do
+
+  local step = function(pos, _)
     local node = minetest.get_node(pos)
-    local facedir = node.param2
-    local newpos = vector.add(pos, minetest.facedir_to_dir(facedir))
-    local newnode = minetest.get_node(newpos)
-    if newnode.name == "worm:body" then
-      newnode.name = "worm:tail"
-      minetest.set_node(newpos, newnode)
-      local timer = minetest.get_node_timer(newpos)
-      timer:set(walking_period, 0)
-    elseif newnode.name == "worm:head" then
-      minetest.set_node(newpos, {name = "air"})
-    end
-    minetest.set_node(pos, {name = "air"})
+    node.name = "worm:tail_"..(n+1)
+    minetest.set_node(pos, node)
+    local timer = minetest.get_node_timer(pos)
+    timer:set(walking_period, 0)
     return false
-  end,
-  after_dig_node = function(pos, node, _, _)
-    local newpos = vector.add(pos, minetest.facedir_to_dir(node.param2))
-    local newnode = minetest.get_node(newpos)
-    if newnode.name == "worm:body" then
-      newnode.name = "worm:tail"
-      minetest.set_node(newpos, newnode)
-      local timer = minetest.get_node_timer(newpos)
-      timer:set(walking_period, 0)
-    elseif newnode.name == "worm:head"  then
-      newnode.name = "air"
-      minetest.set_node(newpos, newnode)
+  end
+  
+  if n == walking_steps then
+  step =  function(pos, elapsed)
+      local node = minetest.get_node(pos)
+      local facedir = node.param2
+      local newpos = vector.add(pos, minetest.facedir_to_dir(facedir))
+      local newnode = minetest.get_node(newpos)
+      if newnode.name == "worm:body" then
+        newnode.name = "worm:tail_1"
+        minetest.set_node(newpos, newnode)
+        local timer = minetest.get_node_timer(newpos)
+        timer:set(walking_period, 0)
+      elseif newnode.name:find("worm:head") == 1 then
+        minetest.set_node(newpos, {name = "air"})
+      end
+      minetest.set_node(pos, {name = "air"})
+      return false
     end
-  end,
-  groups = {snappy = 3},
-  drop = "",
-})
+  end
+
+  minetest.register_node("worm:tail_"..n, {
+    description = "A worms tail",
+    paramtype = "light",
+    paramtype2 = "facedir",
+    drawtype = "nodebox",
+    node_box = {
+        type = "fixed",
+        fixed = {
+          { -0.5, -0.5, -0.5 + (1 / walking_steps) * (n - 1), 0.5, 0.5, 0.5},
+        },
+    },
+    tiles = {
+      "worm_side.png^[transform1",
+      "worm_side.png^[transform1",
+      "worm_side.png",
+      "worm_side.png",
+      "worm_side.png",
+      "worm_tail.png"
+    },
+    on_timer = step,
+    after_dig_node = function(pos, node, _, _)
+      local newpos = vector.add(pos, minetest.facedir_to_dir(node.param2))
+      local newnode = minetest.get_node(newpos)
+      if newnode.name == "worm:body" then
+        newnode.name = "worm:tail_1"
+        minetest.set_node(newpos, newnode)
+        local timer = minetest.get_node_timer(newpos)
+        timer:set(walking_period, 0)
+      elseif newnode.name:find("worm:head") == 1  then
+        newnode.name = "air"
+        minetest.set_node(newpos, newnode)
+      end
+    end,
+    groups = {snappy = 3},
+    drop = "",
+  })
+
+end
 
 
 
