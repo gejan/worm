1e7d7b1e6f2c12b08d09f69952f92498e845785a
Jannik Kulesha <s9jakule@stud.uni-saarland.de>
2017-04-08 23:27:24 +0200

    rewrite spawning, add snake

---
+++ README.txt
@@ -0,0 +1,19 @@
+Minetest Worm Mod
+--------------------------
+
+Adds moving worms and snakes
+
+Depends on: 
+Default (minetest_game) optional, only for placement of "default:dirt"
+
+
+LICENSE:
+
+Copyright (C) 2016 Jannik Kulesha 
+
+Code: 
+LGPL v3
+
+Textures: 
+CC-0
+  
--- init.lua
+++ init.lua
@@ -1,49 +1,75 @@
 -- Minetest Worm Mod
 -- by Gerald
 
-local walking_period = 1  -- seconds
-local walking_steps = 4     -- needs to be bigger than 0
+local MODNAME = "worm"
 
-minetest.register_craftitem("worm:spawnegg", {
-  description = "worm",
-  inventory_image = "worm_face.png",
+local WALKING_PERIOD = 1      -- seconds
+local WALKING_STEPS = 4       -- needs to be bigger than 0
+local TAIL_PROTECTION = false -- if false check for protection only at head position 
+
+
+
+local register_worm = function(modname, name, leftover, face, side, tail, can_move)
+
+minetest.register_craftitem(modname..":"..name.."_spawnegg", {
+  description = "spawn "..name,
+  inventory_image = face,
   on_use = function(itemstack, user, pointed_thing)
     if pointed_thing.type ~= "node" then
       return itemstack
     end
-    local pos = pointed_thing.above
-    local dir = 0
-    local length = itemstack:get_count()
-    minetest.set_node(pos, {name = "worm:head_1", param2 = dir})
-    local headtimer = minetest.get_node_timer(pos)
-    pos.z = pos.z - 1
-    for i = 0, length do
-      minetest.set_node(pos, {name = "worm:body", param2 = dir})
-      pos.z = pos.z - 1
+    local pos = pointed_thing.above 
+    local player_name = user:get_player_name()
+    if minetest.is_protected(pos, player_name) then
+      return itemstack
     end
-    minetest.set_node(pos, {name = "worm:tail_1", param2 = dir})
+    if minetest.get_node(pos).name ~= "air" then
+      return itemstack
+    end
+    
+    local player_dir = vector.subtract({x = 0, y = 0, z = 0}, user:get_look_dir())
+    local facedir = minetest.dir_to_facedir(player_dir)
+    local dir = minetest.facedir_to_dir(facedir)
+    
+    local length = itemstack:get_count()
+    minetest.set_node(pos, {name = modname..":"..name.."_head_1", param2 = facedir})
+    local headtimer = minetest.get_node_timer(pos)
+    -- pos.z = pos.z - 1
+    pos = vector.subtract(pos, dir)
+    local i = 0
+    while i < length do
+      if minetest.get_node(pos).name ~= "air" or (TAIL_PROTECTION and minetest.is_protected(pos, player_name)) then
+        pos = vector.add(pos, dir)
+        minetest.set_node(pos, {name = modname..":"..name.."_tail_1", param2 = facedir})
+        break
+      end
+      minetest.set_node(pos, {name = modname..":"..name.."_body", param2 = facedir})
+      pos = vector.subtract(pos, dir)
+      i = i + 1
+    end
+    minetest.set_node(pos, {name = modname..":"..name.."_tail_1", param2 = facedir})
     local tailtimer = minetest.get_node_timer(pos)
     
-    headtimer:set(walking_period, 0)
-    tailtimer:set(walking_period, 0)
+    headtimer:set(WALKING_PERIOD, 0)
+    tailtimer:set(WALKING_PERIOD, 0)
     
-    itemstack:clear()
+    itemstack:take_item(i)
     return itemstack
   end,
 })
 
-for n = 1, walking_steps do
+for n = 1, WALKING_STEPS do
 
   local step = function(pos, _)
     local node = minetest.get_node(pos)
-    node.name = "worm:head_"..(n+1)
+    node.name = modname..":"..name.."_head_"..(n+1)
     minetest.set_node(pos, node)
     local timer = minetest.get_node_timer(pos)
-    timer:set(walking_period, 0)
+    timer:set(WALKING_PERIOD, 0)
     return false
   end
   
-  if n == walking_steps then
+  if n == WALKING_STEPS then
     step = function(pos, _)
       local node = minetest.get_node(pos)
       local facedir = node.param2
@@ -53,15 +79,15 @@ for n = 1, walking_steps do
         (facedir + 1) % 4,
         (facedir + 2) % 4,
         (facedir + 3) % 4,
-        5 
+        8 
       }
       for i = 1, 6 do
         local newpos = vector.add(pos, minetest.facedir_to_dir(dirs[i]))
-        if minetest.registered_nodes[minetest.get_node(newpos).name].buildable_to then
-          minetest.set_node(newpos, {name = "worm:head_1", param2 = dirs[i]})
-          minetest.set_node(pos, {name = "worm:body", param2 = dirs[i]})
+        if can_move(newpos) then
+          minetest.set_node(newpos, {name = modname..":"..name.."_head_1", param2 = dirs[i]})
+          minetest.set_node(pos, {name = modname..":"..name.."_body", param2 = dirs[i]})
           local timer = minetest.get_node_timer(newpos)
-          timer:set(walking_period, 0)
+          timer:set(WALKING_PERIOD, 0)
           return false
         end
       end
@@ -71,24 +97,24 @@ for n = 1, walking_steps do
   end
 
 
-  minetest.register_node("worm:head_"..n, {
-    description = "A worms head",
+  minetest.register_node(modname..":"..name.."_head_"..n, {
+    description = name.." head",
     paramtype = "light",
     paramtype2 = "facedir",
     drawtype = "nodebox",
     node_box = {
       type = "fixed",
       fixed = {
-        {-0.5, -0.5, -0.5, 0.5, 0.5, -0.5 + (1 / walking_steps) * n},
+        {-0.5, -0.5, -0.5, 0.5, 0.5, -0.5 + (1 / WALKING_STEPS) * n},
       },
     },
     tiles = {
-      "worm_side.png^[transform1",
-      "worm_side.png^[transform1",
-      "worm_side.png",
-      "worm_side.png",
-      "worm_face.png",
-      "worm_side.png"
+      side.."^[transform1",
+      side.."^[transform3",
+      side,
+      side.."^[transform2",
+      face,
+      side
     },
     groups = {snappy = 1, level = 2, not_in_creative_inventory = 1},
     damage_per_second = 10,
@@ -98,28 +124,28 @@ for n = 1, walking_steps do
   
 end
 
-minetest.register_node("worm:body", {
-  description = "A worms body part",
+minetest.register_node(modname..":"..name.."_body", {
+  description = name.." body part",
   paramtype = "light",
   paramtype2 = "facedir",
   tiles = {
-    "worm_side.png^[transform1",
-    "worm_side.png^[transform1",
-    "worm_side.png",
-    "worm_side.png",
-    "worm_side.png",
-    "worm_side.png"
+    side.."^[transform1",
+    side.."^[transform3",
+    side,
+    side.."^[transform2",
+    side,
+    side
   },
   groups = {snappy = 1, level = 2, not_in_creative_inventory = 1},
   after_dig_node = function(pos, node, _, _)
     local newpos = vector.add(pos, minetest.facedir_to_dir(node.param2))
     local newnode = minetest.get_node(newpos)
-    if newnode.name == "worm:body" then
-      newnode.name = "worm:tail_1"
+    if newnode.name == modname..":"..name.."_body" then
+      newnode.name = modname..":"..name.."_tail_1"
       minetest.set_node(newpos, newnode)
       local timer = minetest.get_node_timer(newpos)
-      timer:set(walking_period, 0)
-    elseif newnode.name:find("worm:head") == 1  then
+      timer:set(WALKING_PERIOD, 0)
+    elseif newnode.name:find(modname..":"..name.."_:head") == 1  then
       newnode.name = "air"
       minetest.set_node(newpos, newnode)
     end
@@ -127,65 +153,65 @@ minetest.register_node("worm:body", {
   drop = "",
 })
 
-for n = 1, walking_steps do
+for n = 1, WALKING_STEPS do
 
   local step = function(pos, _)
     local node = minetest.get_node(pos)
-    node.name = "worm:tail_"..(n+1)
+    node.name = modname..":"..name.."_tail_"..(n+1)
     minetest.set_node(pos, node)
     local timer = minetest.get_node_timer(pos)
-    timer:set(walking_period, 0)
+    timer:set(WALKING_PERIOD, 0)
     return false
   end
   
-  if n == walking_steps then
+  if n == WALKING_STEPS then
   step =  function(pos, elapsed)
       local node = minetest.get_node(pos)
       local facedir = node.param2
       local newpos = vector.add(pos, minetest.facedir_to_dir(facedir))
       local newnode = minetest.get_node(newpos)
-      if newnode.name == "worm:body" then
-        newnode.name = "worm:tail_1"
+      if newnode.name == modname..":"..name.."_body" then
+        newnode.name = modname..":"..name.."_tail_1"
         minetest.set_node(newpos, newnode)
         local timer = minetest.get_node_timer(newpos)
-        timer:set(walking_period, 0)
-      elseif newnode.name:find("worm:head") == 1 then
+        timer:set(WALKING_PERIOD, 0)
+      elseif newnode.name:find(modname..":"..name.."_head") == 1 then
         minetest.set_node(newpos, {name = "air"})
       end
-      minetest.set_node(pos, {name = "air"})
+      minetest.set_node(pos, {name = leftover})
       return false
     end
   end
 
-  minetest.register_node("worm:tail_"..n, {
-    description = "A worms tail",
+  minetest.register_node(modname..":"..name.."_tail_"..n, {
+    description = name.." tail",
     paramtype = "light",
     paramtype2 = "facedir",
     drawtype = "nodebox",
     node_box = {
         type = "fixed",
         fixed = {
-          { -0.5, -0.5, -0.5 + (1 / walking_steps) * (n - 1), 0.5, 0.5, 0.5},
+          { -0.5, -0.5, -0.5 + (1 / WALKING_STEPS) * (n - 1), 0.5, 0.5, 0.5},
         },
     },
     tiles = {
-      "worm_side.png^[transform1",
-      "worm_side.png^[transform1",
-      "worm_side.png",
-      "worm_side.png",
-      "worm_side.png",
-      "worm_tail.png"
+      side.."^[transform1",
+      side.."^[transform3",
+      side,
+      side.."^[transform2",
+      side,
+      tail
     },
     on_timer = step,
     after_dig_node = function(pos, node, _, _)
       local newpos = vector.add(pos, minetest.facedir_to_dir(node.param2))
       local newnode = minetest.get_node(newpos)
-      if newnode.name == "worm:body" then
-        newnode.name = "worm:tail_1"
+      if newnode.name == modname..":"..name.."_body" then
+        newnode.name = modname..":"..name.."_tail_1"
         minetest.set_node(newpos, newnode)
         local timer = minetest.get_node_timer(newpos)
-        timer:set(walking_period, 0)
-      elseif newnode.name:find("worm:head") == 1  then
+        timer:set(WALKING_PERIOD, 0)
+      elseif newnode.name:find(modname..":"..name.."_head") == 1  then
         newnode.name = "air"
         minetest.set_node(newpos, newnode)
       end
@@ -196,6 +222,32 @@ for n = 1, walking_steps do
 
 end
 
+end
 
+
+local worm_leftover = "air"
+if default then
+  worm_leftover = "default:dirt"
+end
+register_worm(MODNAME, "worm", 
+  worm_leftover, 
+  "worm_face.png", 
+  "worm_side.png", 
+  "worm_tail.png",
+  function(pos)
+    local name = minetest.get_node(pos).name
+    return minetest.registered_nodes[name].buildable_to or minetest.get_node_group(name, "crumbly") >= 3
+  end
+  )
+
+register_worm(MODNAME, "snake",
+  "air", 
+  "snake_face.png", 
+  "snake_side.png", 
+  "snake_tail.png", 
+  function(pos)
+    return minetest.registered_nodes[minetest.get_node(pos).name].buildable_to
+  end 
+)
 
 
